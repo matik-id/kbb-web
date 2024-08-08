d8cce068ea9aba57b72642c57c10f51750c40bea
Sultoni DAP <sulthonidap@gmail.com>
2024-08-08 12:51:22 +0700

    try new layout

--- src/components/CardDonate.tsx
+++ src/components/CardDonate.tsx
@@ -56,76 +56,11 @@ export default function CardDonate() {
 
   return (
     <div className="px-4 md:px-36 md:py-10 overflow-hidden bg-white ">
-      <div className="grid grid-cols-1 gap-24 ">
-        
+      <div className="grid grid-cols-1 gap-8 ">       
         <div className="md:w-full ">
-          <h1 className="font-bold text-[#1E8B43] border-l-8 border-[#1E8B43] pl-2 mb-8">
+          <h1 className="font-bold text-[#1E8B43] border-l-8 border-[#1E8B43] pl-2 ">
             Donasi Kemanusiaan
-          </h1>
-          
-          <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
-            {data?.data.records.slice(0, 6).map((item, index) => (
-                <div
-                  key={index}
-                  className="border border-gray-200 rounded-lg shadow-lg hover:shadow-2xl hover:scale-105 transition-all duration-500  gap-3 w-[188px] h-[352px] 
-                cursor-pointer"
-                  onClick={() =>
-                    router.push(
-                      `/product/list`
-                    )
-                  }
-                >
-                  <div className="overflow-hidden rounded-lg object-cover h-[189px]">
-                    <img
-                      src={item.thumbnail}
-                      alt=""
-                      className="h-[189px] w-[188px] object-cover hover:scale-105 transition-all duration-500 "
-                    />
-                  </div>
-                  <div className="p-2">
-                    <h1 className=" text-[#1E8B43] mb-2 font-bold">
-                      {item.title.length > 15 ? `${item.title.slice(0, 15)}...` : item.title }
-                    </h1>
-                    <p className=" text-sm font-bold mb-1">
-                      Rp {item.price.toLocaleString("id-ID")}
-                    </p>
-                    <p className="text-gray-500 text-sm flex items-center gap-1 mb-1">
-                      <FaUserAlt />
-                      {item.owner.slice(0, 10)}
-                      {item.owner.length > 10 ? "..." : ""}
-                    </p>
-                    
-                  </div>
-                  <div className="flex justify-between p-2 items-center">
-                    <p className="text-gray-500 text-sm flex items-center gap-1">
-                      <BsEye />
-                      {item.viewer}
-                    </p>
-                    <button
-                      className="bg-[#84CC16] text-white px-2 py-1 rounded-lg hover:bg-[#84CC16]/90 transition-all duration-500 "
-                      onClick={() =>
-                        router.push(
-                          `/product/list`
-                        )
-                      }
-                    >
-                      Lihat
-                    </button>
-                  </div>
-                </div>
-            ))}
-            </div>
-          <div className="flex flex-col gap-5">
-            <div className="flex justify-end mt-4">
-              <button
-                className="bg-[#84CC16] text-white px-5 py-2 rounded-lg hover:bg-[#84CC16]/90 transition-all duration-500 
-              "
-                onClick={() => (window.location.href = "/product/list")}
-              >
-                Selengkapnya
-              </button>
-            </div>
-          </div>
+          </h1>         
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
@@ -160,6 +95,17 @@ export default function CardDonate() {
           </div>
         ))}
         </div>
+        <div className="flex flex-col gap-5">
+            <div className="flex justify-end ">
+              <button
+                className="bg-[#84CC16] text-white px-5 py-2 rounded-lg hover:bg-[#84CC16]/90 transition-all duration-500 
+              "
+                onClick={() => (window.location.href = "/product/list")}
+              >
+                Selengkapnya
+              </button>
+            </div>
+          </div>
 
 
       </div>
